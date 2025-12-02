a140628ef8a8c859036880a098f496594df24cc7
Jose Machava <jose.machava@vm.co.mz>
2025-12-02 23:02:19 +0200

    chore: add new route in here

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def hello_world():  # put application's code here
 def hello_world():  # put application's code here
     return 'Hello new route flask api!'
 
+@app.route('/new/here')
+def hello_world():  # put application's code here
+    return 'Hello new route flask api!'
+
 if __name__ == '__main__':
     app.run()
